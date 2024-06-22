b4ddb1e7ce28cb27be1c72a8c749f0327d9a2b45
Akella348 <akella348@gmail.com>
2024-06-22 23:47:12 +0400

    Create homework6.py

---
+++ homework6.py
@@ -0,0 +1,12 @@
+my_dict={'Anton':1980, 'Artem': 1993, 'Darya': 1994}
+print(my_dict)
+print(my_dict['Artem'])
+print(my_dict.get('Nikita'))
+my_dict.update({'Valera': 1990, 'Igor': 1991})
+print(my_dict.pop('Anton'))
+print(my_dict)
+my_set={2,3,5,6,7,5,4,3,5,6,7,True, (1,4,5)}
+print(my_set)
+my_set.add(1)
+my_set.remove(5)
+print(my_set)
